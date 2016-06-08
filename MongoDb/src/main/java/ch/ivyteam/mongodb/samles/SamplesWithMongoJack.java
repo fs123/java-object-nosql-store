bc8c419c742bb08547f479836b6e6a21fdb9b139
Flavio <fs@sadeghi.ch>
2016-06-08 14:53:35 +0200

    added first example (with mongojack)

---
+++ MongoDb/src/main/java/ch/ivyteam/mongodb/samles/SamplesWithMongoJack.java
@@ -0,0 +1,64 @@
+package ch.ivyteam.mongodb.samles;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.mongojack.Id;
+import org.mongojack.JacksonDBCollection;
+import org.mongojack.WriteResult;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+/**
+ * First simple example with the test database which is already shipped with a plain mongoDB installation.
+ * <p>
+ * mongojack: https://github.com/mongojack/mongojack
+ */
+public class SamplesWithMongoJack
+{
+  public static void main(String[] args)
+  {
+    MongoClient mongoClient = new MongoClient();
+    DB db = mongoClient.getDB("test");
+    
+    DBCollection collection = db.getCollection("dummy");
+    
+    JacksonDBCollection<MyObject, String> coll = JacksonDBCollection.wrap(collection, MyObject.class, String.class);
+
+    MyObject myObject = createMyObject("1");
+    
+    WriteResult<MyObject, String> result = coll.insert(myObject);
+    String id = result.getSavedId();
+    MyObject savedObject = result.getSavedObject();
+
+    System.out.println("inserted entry with ID="+ id +": " + ToStringBuilder.reflectionToString(savedObject));
+  }
+  
+  private static MyObject createMyObject(String key) {
+    MyObject myObject = new MyObject();
+    myObject.stringValue = "ABC-" + key;
+    myObject.numberValue = 123;
+    myObject.child1 = new MyChildObject();
+    myObject.child1.strValue = "A" + key;
+    myObject.child2 = new MyChildObject();
+    myObject.child2.strValue = "B" + key;
+    return myObject;
+  }
+  
+  public static class MyObject {
+    @Id
+    public String id;
+    
+    public String stringValue;
+    public Number numberValue;
+    
+    public MyChildObject child1;
+    public MyChildObject child2;
+    
+  }
+
+  public static class MyChildObject {
+    public String strValue;
+  }
+
+}
