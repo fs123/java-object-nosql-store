a5f56910689b3b18ca164ef4dd71ecc4d33b3313
ivy-rew <reguel.wermelinger@axonivy.com>
2016-06-13 14:33:46 +0200

    test read query under 3 s

--- Neo4j/src/test/java/ch/ivyteam/java/object/store/data/TestManyDocuments.java
+++ Neo4j/src/test/java/ch/ivyteam/java/object/store/data/TestManyDocuments.java
@@ -3,6 +3,7 @@ package ch.ivyteam.java.object.store.data;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
+import java.util.Set;
 
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.FixMethodOrder;
@@ -10,8 +11,11 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.neo4j.ogm.session.Session;
 
+import ch.ivyteam.college.neo4j.v2.Course;
+import ch.ivyteam.college.neo4j.v2.Enrollment;
 import ch.ivyteam.college.neo4j.v2.School;
 import ch.ivyteam.college.neo4j.v2.Student;
+import ch.ivyteam.college.neo4j.v2.Subject;
 import ch.ivyteam.java.object.store.Documents;
 import ch.ivyteam.java.object.store.GraphDocPersistency;
 import ch.ivyteam.neo4j.Neo4jSessionFactory;
@@ -53,15 +57,22 @@ public class TestManyDocuments
   public void test3_loadComplexTree()
   {
     StopWatch stopWatch = new StopWatch();
-    stopWatch.start();
     
     Session session = Neo4jSessionFactory.getInstance().getNeo4jSessionV2();
-    School aSchool = session.load(School.class, 4174l, Integer.MAX_VALUE);
-    assertThat(aSchool).isNotNull();
-    assertThat(aSchool.getDepartments().iterator().next().getSubjects()).isNotEmpty();
+    stopWatch.start();
+    int LOAD_DEPTH = 3; // raise it further to see damn high CPU usage during object mapping :-)
+    School aSchool = session.load(School.class, 4174l, LOAD_DEPTH);
     stopWatch.stop();
+    assertThat(aSchool).isNotNull();
+    Set<Subject> subjects = aSchool.getDepartments().iterator().next().getSubjects();
+    assertThat(subjects).isNotEmpty();
+    Set<Course> courses = subjects.iterator().next().getCourses();
+    assertThat(courses).isNotEmpty();
+    Set<Enrollment> enrollments = courses.iterator().next().getEnrollments();
+    assertThat(enrollments).isNotEmpty();
+    assertThat(enrollments.iterator().next().getStudent()).isNotNull();
     
-    assertThat(stopWatch.getTime()).isLessThanOrEqualTo(1000);
+    assertThat(stopWatch.getTime()).isLessThanOrEqualTo(3000);
   }
   
 }
